f8b208350abb87ea3daf4f97932014d61c8ba74b
Ant Zucaro <azucaro@gmail.com>
2012-11-16 20:44:11 -0500

    Minor output changes.

--- print_gallery.py
+++ print_gallery.py
@@ -10,10 +10,10 @@ prefix = sys.argv[1]
 print '<html>'
 print '<head></head>'
 print '<body>'
-print '<div class="imgblock">'
+print '<div class="gallery">'
 
 for file in files:
-    print '    <a rel="cbimage" href="/%s/%s"><img src="/%s/thumb/%s"/></a>' % (prefix, file, prefix, file)
+    print '    <a href="http://media.antzucaro.com/galleries/%s/%s"><img src="http://media.antzucaro.com/galleries/%s/thumb/%s"/></a>' % (prefix, file, prefix, file)
 
 print '</div>'
 print '</body>'
